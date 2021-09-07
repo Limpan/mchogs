5b267a7694be761ff919b0cdbb1ee2f9c5a1b893
Linus Törngren <linus@etnolit.se>
2021-09-07 22:26:46 +0200

    Improved test coverage.

--- src/test/java/se/etnolit/mchogs/CommandCakeTest.java
+++ src/test/java/se/etnolit/mchogs/CommandCakeTest.java
@@ -2,12 +2,15 @@
 package se.etnolit.mchogs;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -27,5 +30,18 @@ class CommandCakeTest {
         subject.onCommand(player, command, "/cake", new String[0]);
 
         verify(inventory).addItem(any(ItemStack.class));
+    }
+
+    @Test
+    void testWontAllowConsole() {
+        CommandExecutor subject = new CommandCake();
+
+        ConsoleCommandSender console = mock(ConsoleCommandSender.class);
+        
+        Command command = mock(Command.class);
+
+        subject.onCommand(console, command, "/cake", new String[0]);
+
+        verify(console, atLeastOnce()).sendMessage("Only players can have cake!");
     }
 }
